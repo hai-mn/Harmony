d0b80a3c9dcf2a466b151f550de36625987dddcb
hai-mn <drminhhai@gmail.com>
2021-03-24 15:36:15 -0500

    Adjusted legends for IRC plots

--- R/ircplot.R
+++ R/ircplot.R
@@ -1,6 +1,6 @@
 ## Generating Item Response (Characteristic) Curve Plots
 #' @title irc
-#' @description Generating Cumulative and Category Item Response (Characteristic) Curve Plots
+#' @description Generating Category and Cumulative Probability Curve Plots
 #' @details irc requires the user to firstly run the 'alignmentout' and 'convert2irt' to obtain the difficulty and discrimination parameters
 #' @author Hai Nguyen \email{hnguye72@@uic.edu}, Ariel Aloe, Tianxiu Wang, Rachel Gordon
 #' @export irc
@@ -70,95 +70,95 @@ irc <- function(){
   # Select the item to plot
   selected.item.n <- utils::menu(Item.name, title = "Input an item need to be plotted: ")
   selected.item <- as.character(Item.name[selected.item.n])
-  
+
   # Select group(s)
-  
-  
+
+
   selected.group.option <- utils::menu(c(Group.cat, "Other Combination"), title="Input the Group(s): ")
-  
+
   # Plot for other combination ===============================================================
   if (selected.group.option == (Group + 1)) {
     selected.group.line <- readline(prompt="Enter group number (separated by ,):\n")
     selected.group <- as.character(str_squish(unlist(strsplit(selected.group.line, ","))))
-    
+
     # plotting a frame cumulative curve
     irc <-ggplot(data.frame(theta = c(-4, 4)), aes(x = theta)) +
       xlab(latex2exp::TeX("$\\theta")) +
       ylab("Probability") +
-      labs(title=paste("Cumulative Probability Curves for Group", selected.group.line, "of Item", selected.item), caption = "") +
+      labs(title=paste("Cumulative Probability Curves \nfor Group", selected.group.line, "of Item", selected.item), caption = "The multiple lines reflect different subgroups") +
       theme_bw()
-    
+
     # plotting a frame category curve
     circ <-ggplot(data.frame(theta = c(-4, 4)), aes(x = theta)) +
       xlab(latex2exp::TeX("$\\theta")) +
       ylab("Probability") +
-      labs(title=paste("Category Probability Curves for Group", selected.group.line, "of Item", selected.item), caption = "The multiple lines reflect different subgroups") +
+      labs(title=paste("Category Probability Curves \nfor Group", selected.group.line, "of Item", selected.item), caption = "") +
       theme_bw()
-    
+
     for (k in 1:length(selected.group)) {
-      
+
       i <- selected.group[k]
-      
+
       keep.group <- paste0("G",i,"$")
       keep.var <- names(total[,grep(keep.group, names(total), value=TRUE)])
       item[[i]] <- total %>% dplyr::filter(Item == selected.item) %>%
       dplyr::select(., which(names(.) %in% keep.var))
-    
+
       for (j in 2:(Threshold.max+1)){
         irc <- irc + stat_function(fun = logisticFun, args = list(a=as.numeric(item[[i]][1]),b=as.numeric(item[[i]][j])))
       }
-      
+
       circ <- circ +
         stat_function(fun = pg0, args = list(a=as.numeric(item[[i]][1]),b=as.numeric(item[[i]][2]))) +
         stat_function(fun = pgl, args = list(a=as.numeric(item[[i]][1]),b=as.numeric(item[[i]][Threshold.max+1])))
-      
-      
+
+
       for (j in 2:(Threshold.max)){
         circ <- circ + stat_function(fun = pgbtwn, args = list(a1=as.numeric(item[[i]][1]), b1=as.numeric(item[[i]][j]), a2=as.numeric(item[[i]][1]), b2=as.numeric(item[[i]][j+1])))
       }
     }
 
   } else {
-    
+
     # Plot for one group ===============================================================
     # plotting a frame cumulative curve
     irc <-ggplot(data.frame(theta = c(-4, 4)), aes(x = theta)) +
       xlab(latex2exp::TeX("$\\theta")) +
       ylab("Probability") +
-      labs(title=paste("Cumulative Probability Curves for Group", selected.group.option, "of Item", selected.item), caption = "") +
+      labs(title=paste("Cumulative Probability Curves \nfor Group", selected.group.option, "of Item", selected.item), caption = "The multiple lines reflect different subgroups") +
       theme_bw()
-      
+
     # plotting a frame category curve
     circ <-ggplot(data.frame(theta = c(-4, 4)), aes(x = theta)) +
       xlab(latex2exp::TeX("$\\theta")) +
       ylab("Probability") +
-      labs(title=paste("Category Probability Curves for Group", selected.group.option, "of Item", selected.item), caption = "The multiple lines reflect different subgroups") +
+      labs(title=paste("Category Probability Curves \nfor Group", selected.group.option, "of Item", selected.item), caption = "") +
       theme_bw()
-    
+
     # process data for plot
       i <- selected.group <- selected.group.option
       keep.group <- paste0("G",i,"$")
       keep.var <- names(total[,grep(keep.group, names(total), value=TRUE)])
       item[[i]] <- total %>% dplyr::filter(Item == selected.item) %>%
         dplyr::select(., which(names(.) %in% keep.var))
-      
+
     # overlay irc
     for (j in 2:(Threshold.max+1)){
       irc <- irc + stat_function(fun = logisticFun, args = list(a=as.numeric(item[[i]][1]),b=as.numeric(item[[i]][j])))
     }
-      
+
     # overlay circ
     circ <- circ +
       stat_function(fun = pg0, args = list(a=as.numeric(item[[i]][1]),b=as.numeric(item[[i]][2]))) +
       stat_function(fun = pgl, args = list(a=as.numeric(item[[i]][1]),b=as.numeric(item[[i]][Threshold.max+1])))
-      
-      
+
+
     for (j in 2:(Threshold.max)){
       circ <- circ + stat_function(fun = pgbtwn, args = list(a1=as.numeric(item[[i]][1]), b1=as.numeric(item[[i]][j]), a2=as.numeric(item[[i]][1]), b2=as.numeric(item[[i]][j+1])))
     }
-    
+
   }
-  
+
   figure <- ggpubr::ggarrange(circ, irc, ncol = 1, nrow = 2)
   figure
 }
